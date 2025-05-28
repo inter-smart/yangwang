37ee706ce00e2d74d4363304f839bf83bee7b0a8
ajayux <ajay@intersmart.in>
2025-05-28 14:39:52 +0530

    corrections

--- src/app/contact/page.js
+++ src/app/contact/page.js
@@ -6,7 +6,7 @@ export default function Contact() {
   return (
     <>
       <EnquirySection /> 
-      <LocationSection /> 
+      {/* <LocationSection />  */}
       <FaqSection /> 
     </>
   );
