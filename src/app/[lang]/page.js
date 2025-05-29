7965dd0e14cfbb838ae02a69ce6659708da9d571
Vaishnav-Vijayan07 <vaishnav@intersmart.in>
2025-05-29 10:23:07 +0530

    feat: Reintroduce About, Legal Statement, News, Offers, and Privacy Policy pages with updated components

--- src/app/[lang]/page.js
+++ src/app/[lang]/page.js
@@ -1,3 +1,4 @@
+'use client'
 import HeroSection from "@/components/features/home/HeroSection";
 import AboutSection from "@/components/features/home/AboutSection";
 import ModelSection from "@/components/features/home/ModelSection";
@@ -6,8 +7,13 @@ import LaunchOffersSection from "@/components/features/home/LaunchOffersSection"
 import InteriorSection from "@/components/features/home/InteriorSection";
 import MarketingSection from "@/components/features/home/MarketingSection";
 import EventsSection from "@/components/features/home/EventsSection";
+import { useParams } from "next/navigation";
 
 export default function Home() {
+  const {lang} = useParams();
+
+  console.log("lang", lang);
+  
   return (
     <>
       <HeroSection />
