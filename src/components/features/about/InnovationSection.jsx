5c50a5c06303e1def51fe71b83be3f783b8a234d
Vaishnav-Vijayan07 <vaishnav@intersmart.in>
2025-05-30 17:41:33 +0530

    feat: enhance InnovationSection with dynamic data handling and image support

--- src/components/features/about/InnovationSection.jsx
+++ src/components/features/about/InnovationSection.jsx
@@ -7,6 +7,7 @@ import { Autoplay } from "swiper/modules";
 import ModalVideo from "react-modal-video";
 import "react-modal-video/css/modal-video.css";
 import "swiper/css";
+import Image from "next/image";
 
 const videoReviews = [
   {
@@ -31,17 +32,14 @@ const videoReviews = [
 
 export default function InovationSection({ locale, data }) {
   console.log("data inovation =====>", data);
-  
+  const { items, title } = data;
+
   const [videoUrl, setVideoUrl] = useState(null);
   return (
     <section className="w-full h-auto block 2xl:p-[145px_0_190px] xl:p-[100px_0_110px] lg:p-[60px_0_70px] p-[40px_0_50px] bg-[#1D0A44]">
       <div className="container">
-        <Heading
-          size="heading3"
-          as="h3"
-          className="text-white mb-[20px] md:mb-[30px] xl:mb-[50px] 2xl:mb-[70px]"
-        >
-          Innovation Across the Lineup
+        <Heading size="heading3" as="h3" className="text-white mb-[20px] md:mb-[30px] xl:mb-[50px] 2xl:mb-[70px]">
+          {title}
         </Heading>
         <Swiper
           key={locale}
@@ -64,25 +62,25 @@ export default function InovationSection({ locale, data }) {
             },
           }}
         >
-          {videoReviews.map((item, index) => (
+          {items?.map((item, index) => (
             <SwiperSlide key={index}>
               <div
                 className="w-full h-full block overflow-hidden relative z-0 before:content-[''] before:absolute before:z-1 before:bottom-0 before:w-full before:h-[50%] before:bg-gradient-to-b before:from-[rgba(34,34,34,0)] before:to-[rgba(21,20,20,0.9)] group"
-                onClick={() => setVideoUrl(item.videoUrl)}
+                onClick={() => setVideoUrl(item.video)}
                 role="button"
                 aria-label={`Play video: ${item.title}`}
               >
-                <Img
-                  src={item.thumbnail}
-                  alt={item.title}
+                <Image
+                  src={item.video_thumbnail}
+                  alt={item.alt}
                   width={550}
                   height={415}
                   className="object-cover group-hover:scale-105 transition-transform duration-300"
                 />
                 <div className="w-full md:gap-[15px] gap-[10px] 2xl:p-[50px_35px] xl:p-[20px_15px] md:p-[15px_10px] p-[10px] absolute z-1 bottom-0 left-0 flex items-center">
                   <div className="lg:w-[45px] w-[25px]">
-                    <Img
-                      src="inovation-play.png"
+                    <Image
+                      src={item?.video_thumbnail}
                       alt="inovation-play"
                       width={45}
                       height={45}
