461de0a4f01b4a75bd2ea5dab1ccdc48563bfc12
Vaishnav-Vijayan07 <vaishnav@intersmart.in>
2025-06-02 18:23:54 +0530

    feat: implement dynamic metadata generation and improve legal statement and privacy policy components

--- src/app/[locale]/privacy-policy/page.js
+++ src/app/[locale]/privacy-policy/page.js
@@ -1,17 +1,70 @@
-
 import InnerBanner from "@/components/common/InnerBanner";
 import PrivacyPolicy from "@/components/features/privacy-policy/PrivacyPolicy";
 
-export const metadata = {
-    title: "YANGWANG | Privacy policy",
-    description: "Understand how iChargeOn collects.",
-};
+export async function generateMetadata({ params }) {
+  const { locale } = params;
 
-export default function page() {
-    return (
-        <>
-            <InnerBanner title="Privacy Policy" image="privacy-banner.jpg" />
-            <PrivacyPolicy />
-        </>
+  try {
+    const response = await fetch(
+      `${process.env.NEXT_PUBLIC_API_BASE_URL}/privacy-policy?lang=${encodeURIComponent(locale)}`,
+      {
+        cache: "force-cache",
+        next: { revalidate: 60 },
+      }
     );
+    const result = await response.json();
+
+    if (result.success && result.status === 200) {
+      const seoData = result.data?.meta?.seo;
+      return {
+        title: seoData?.title,
+        description: seoData?.description,
+        keywords: seoData?.keywords,
+        openGraph: {
+          title: seoData?.og_title,
+          description: seoData?.og_description,
+          images: seoData?.og_image ? [seoData.og_image] : [],
+          url: seoData?.canonical_url,
+        },
+        alternates: {
+          canonical: seoData?.canonical_url,
+        },
+      };
+    }
+  } catch (error) {
+    console.error("Error generating metadata:", error);
+  }
+}
+
+export default async function page({ params }) {
+  const { locale } = await params;
+
+  let privacyPolicyData = {};
+
+  try {
+    const response = await fetch(`${process.env.NEXT_PUBLIC_API_BASE_URL}/privacy-policy?lang=${encodeURIComponent(locale)}`, {
+      cache: "force-cache",
+      next: { revalidate: 60 },
+    });
+
+    const result = await response.json();
+    if (result.success && result.status === 200) {
+      privacyPolicyData = result.data;
+      console.log(`[2025-05-29T14:37:00.000Z] Fetched ownership  data for ${locale}`, privacyPolicyData);
+    } else {
+      console.error(`[2025-05-29T14:37:00.000Z] API error: ${result.message || "Unknown error"}`);
+    }
+  } catch (error) {
+    console.error(`[2025-05-29T14:37:00.000Z] Failed to fetch ownership data: ${error.message}`);
+  }
+  return (
+    <>
+      <InnerBanner
+        title={privacyPolicyData?.banner_section?.title}
+        banner_image={privacyPolicyData?.banner_section?.web_banner}
+        banner_alt={privacyPolicyData?.banner_section?.web_banner_alt}
+      />
+      <PrivacyPolicy data={privacyPolicyData?.['privacy-policy']} />
+    </>
+  );
 }
