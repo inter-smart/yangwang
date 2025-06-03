f0153cd3ccdc715358d840a2e2beb7b61cce9566
Vaishnav-Vijayan07 <vaishnav@intersmart.in>
2025-06-03 23:05:02 +0530

    feat: implement async data fetching for offers and enhance InnerHeroSection with dynamic data

--- src/app/[locale]/offers/page.js
+++ src/app/[locale]/offers/page.js
@@ -3,10 +3,30 @@ import InnerHeroSection from "@/components/features/offers/InnerHeroSection";
 import OfferInfoSection from "@/components/features/offers/OfferInfoSection";
 import QuestionSection from "@/components/features/service/QuestionSection";
 
-export default function page() {
+export default async function page({params}) {
+  const { locale } = await params;
+
+  let offersData = {};
+
+  try {
+    const response = await fetch(`${process.env.NEXT_PUBLIC_API_BASE_URL}/offers?lang=${encodeURIComponent(locale)}`, {
+      cache: "force-cache",
+      next: { revalidate: 60 },
+    });
+
+    const result = await response.json();
+    if (result.success && result.status === 200) {
+      offersData = result.data;
+      console.log(`[2025-05-29T14:37:00.000Z] Fetched offers  data for ${locale}`, offersData);
+    } else {
+      console.error(`[2025-05-29T14:37:00.000Z] API error: ${result.message || "Unknown error"}`);
+    }
+  } catch (error) {
+    console.error(`[2025-05-29T14:37:00.000Z] Failed to fetch offers data: ${error.message}`);
+  }
   return (
     <>
-      <InnerHeroSection />
+      <InnerHeroSection data={offersData?.banner_section} />
       <OfferInfoSection />
       <QuestionSection />
       <SocialLinkSection />
