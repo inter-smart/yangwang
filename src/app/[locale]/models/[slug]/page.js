7dfc3bb9dbaa10904f59b20bd63fe64024d10f55
Vaishnav-Vijayan07 <vaishnav@intersmart.in>
2025-06-05 13:22:16 +0530

    feat: Uncomment HighlightSection in the page component for improved visibility

--- src/app/[locale]/models/[slug]/page.js
+++ src/app/[locale]/models/[slug]/page.js
@@ -13,7 +13,7 @@ export default async function page({ params }) {
   return (
     <>
       <ModelsHeroSection />
-      {/* <HighlightSection /> */}
+      <HighlightSection />
       <DesignViewSection locale={locale} />
       <RangeSection />
       <PerformanceSection />
